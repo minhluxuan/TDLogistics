99b58f16e952bfdd93cc2458cbbe32ed45c4a5ae
Minh Lu Xuan <121866475+minhluxuan@users.noreply.github.com>
2024-01-01 23:04:32 +0700

    Update otpRoute.js

--- src/routes/otpRoute.js
+++ src/routes/otpRoute.js
@@ -38,7 +38,7 @@ router.get("/otp_success", otpController.verifyOTPSuccess);
 router.post("/test", (req, res) => {
     console.log(req.body.phone_number);
     console.log(req.body.otp);
-}
+});
             
 passport.serializeUser(utils.setSession);
 passport.deserializeUser((user, done) => {
