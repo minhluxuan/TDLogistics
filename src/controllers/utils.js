5be206ddb90b85b3cc709ca41305d64fb79a5796
Minh Lu Xuan <121866475+minhluxuan@users.noreply.github.com>
2024-01-04 10:55:02 +0700

    Update utils.js

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -68,7 +68,6 @@ class CustomerUserRequestValidation {
     validateUpdatingOrder = () => {
         const schema = Joi.object({
             order_id: Joi.string().alphanum().min(5).max(15).required(),
-            order_time: Joi.date(),
             mass: Joi.number().precision(2).min(0),
             height: Joi.number().precision(2).min(0),
             width: Joi.number().precision(2).min(0),
