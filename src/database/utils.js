7572d40996e1acb88de09f4a736029e418aaaf61
Minh Lu Xuan <121866475+minhluxuan@users.noreply.github.com>
2024-01-09 14:10:31 +0700

    Update utils.js

--- src/database/utils.js
+++ src/database/utils.js
@@ -76,10 +76,57 @@ const getLastRow = async (pool, table) => {
     }
 }
 
+const deleteOne = async (pool, table, fields, values, currentTime = null) =>
+{
+    let query;
+    const fieldCondition =  fields.map(field => `${field} = ? `).join(' AND ');
+    if (currentTime !== null) 
+    {
+        const timeCondition = `order_time < = ${currentTime}`; 
+        query = `DELETE FROM ${table} WHERE ${fieldCondition} AND ${timeCondition}  LIMIT 1`;
+    }
+    else
+    {
+        query = `DELETE FROM ${table} WHERE ${fieldCondition} LIMIT 1`;
+    }
+
+    try {
+        const result = await pool.query(query, values);
+        console.log("Success!");
+        return result;
+    } 
+    catch (error) {
+        console.log("Error: ", error);
+        throw "Đã xảy ra lỗi. Vui lòng thử lại sau ít phút!";
+    }
+}
+
+const deleteMany = async  (pool, table, fields = null, values = null) => {
+    let query;
+    if (fields !== null && values !== null) {
+      const conditions =  fields.map(field => `${field} = ? `).join(' AND ');
+      query = `DELETE FROM ${table} WHERE ${conditions}`;
+    }
+    else {
+        query = `DELETE FROM ${table}`;
+    }
+    
+    try {
+        const result = await pool.query(query, values);
+        console.log("Success!");
+        return result.affectedRows;
+    } catch (error) {
+        console.log("Error: ", error);
+        throw "Đã xảy ra lỗi. Vui lòng thử lại sau ít phút!";
+    }
+}
+
 module.exports = {
     findOne,
     find,
     insert,
     update,
     getLastRow,
+    deleteOne,
+    deleteMany,
 }
