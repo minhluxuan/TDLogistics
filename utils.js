aff938e7fb36ebde9edb502ef12a20f69007c210
nhan6104 <thanhan.nguyen6104@gmail.com>
2024-01-21 13:46:32 +0700

    update

--- utils.js
+++ utils.js
@@ -6,11 +6,8 @@ const setSession = (user, done) => {
 }
 
 const verifyPermission = (user, done) => {
-    if (user.permission > 0) {
-        user.active=true;
+    
         return done(null, user);
-    }
-    done(null, false);
 }
 
 const isAuthenticated = (permission) => {
